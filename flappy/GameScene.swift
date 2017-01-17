e22c5940b393b4c000298739bf24cd98f3dbf432
Anand Mukut Tirkey <anand@Anands-MacBook-Pro.local>
2017-01-17 17:29:05 +0530

    code updated by Xcode 8

--- flappy/GameScene.swift
+++ flappy/GameScene.swift
@@ -24,7 +24,7 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
     var moveAndRemove = SKAction()
     var gameStarted = Bool()
     
-    override func didMoveToView(view: SKView) {
+    override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
         scoreLabel.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2 + self.frame.height/3)
         scoreLabel.zPosition = 4
@@ -32,12 +32,12 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         ground = SKSpriteNode(imageNamed: "ground")
         ground.setScale(0.5)
         ground.position = CGPoint(x: self.frame.width/2, y: ground.frame.height/2)
-        ground.physicsBody = SKPhysicsBody(rectangleOfSize: ground.size)
+        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
         ground.physicsBody?.categoryBitMask = physicsCategory.ground
         ground.physicsBody?.collisionBitMask = physicsCategory.bird
         ground.physicsBody?.contactTestBitMask = physicsCategory.bird
         ground.physicsBody?.affectedByGravity = false
-        ground.physicsBody?.dynamic = false
+        ground.physicsBody?.isDynamic = false
         ground.zPosition = 3
         self.addChild(ground)
         
@@ -49,40 +49,40 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         bird.physicsBody?.collisionBitMask = physicsCategory.ground | physicsCategory.wall
         bird.physicsBody?.contactTestBitMask = physicsCategory.scoreNode | physicsCategory.ground | physicsCategory.wall
         bird.physicsBody?.affectedByGravity = false
-        bird.physicsBody?.dynamic = true
+        bird.physicsBody?.isDynamic = true
         bird.zPosition = 2
         
         self.addChild(bird)
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
        /* Called when a touch begins */
         if gameStarted == false{
             gameStarted = true
             bird.physicsBody?.affectedByGravity = true
-            let spawn = SKAction.runBlock({
+            let spawn = SKAction.run({
                 () in
                 self.createWalls()
             })
-            let delay = SKAction.waitForDuration(2.0)
+            let delay = SKAction.wait(forDuration: 2.0)
             let spawnDelay = SKAction.sequence([spawn,delay])
-            let spawnDelayForever = SKAction.repeatActionForever(spawnDelay)
-            self.runAction(spawnDelayForever)
+            let spawnDelayForever = SKAction.repeatForever(spawnDelay)
+            self.run(spawnDelayForever)
             
             let distance = CGFloat(self.frame.width + 20)
-            let movePipes = SKAction.moveByX(-distance, y: 0, duration: 0.01 * Double(distance))
+            let movePipes = SKAction.moveBy(x: -distance, y: 0, duration: 0.01 * Double(distance))
             let removePipes = SKAction.removeFromParent()
             moveAndRemove = SKAction.sequence([movePipes,removePipes])
             
-            bird.physicsBody?.velocity = CGVectorMake(0, 0)
-            bird.physicsBody?.applyImpulse(CGVectorMake(0, 35))
+            bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 35))
         }else{
-            bird.physicsBody?.velocity = CGVectorMake(0, 0)
-            bird.physicsBody?.applyImpulse(CGVectorMake(0, 35))
+            bird.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 35))
         }
     }
    
-    override func update(currentTime: CFTimeInterval) {
+    override func update(_ currentTime: TimeInterval) {
         /* Called before each frame is rendered */
     }
     
@@ -95,12 +95,12 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         let scoreNode = SKSpriteNode()
         scoreNode.size = CGSize(width: 1,height: 200)
         scoreNode.position = CGPoint(x: self.frame.width ,y: self.frame.height/2)
-        scoreNode.physicsBody = SKPhysicsBody(rectangleOfSize: scoreNode.size)
+        scoreNode.physicsBody = SKPhysicsBody(rectangleOf: scoreNode.size)
         scoreNode.physicsBody?.categoryBitMask = physicsCategory.scoreNode
         scoreNode.physicsBody?.collisionBitMask = 0
         scoreNode.physicsBody?.contactTestBitMask = physicsCategory.bird
         scoreNode.physicsBody?.affectedByGravity = false
-        scoreNode.physicsBody?.dynamic = false
+        scoreNode.physicsBody?.isDynamic = false
         //scoreNode.color = SKColor.blueColor()
 
         topWall.position = CGPoint(x: self.frame.width, y: self.frame.height/2 + 350)
@@ -108,18 +108,18 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         topWall.setScale(0.5)
         bottomWall.setScale(-0.5)
         
-        topWall.physicsBody = SKPhysicsBody(rectangleOfSize: topWall.size)
+        topWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
         topWall.physicsBody?.categoryBitMask = physicsCategory.wall
         topWall.physicsBody?.collisionBitMask = physicsCategory.bird
         topWall.physicsBody?.contactTestBitMask = physicsCategory.bird
-        topWall.physicsBody?.dynamic = false
+        topWall.physicsBody?.isDynamic = false
         topWall.physicsBody?.affectedByGravity = false
         
-        bottomWall.physicsBody = SKPhysicsBody(rectangleOfSize: bottomWall.size)
+        bottomWall.physicsBody = SKPhysicsBody(rectangleOf: bottomWall.size)
         bottomWall.physicsBody?.categoryBitMask = physicsCategory.wall
         bottomWall.physicsBody?.collisionBitMask = physicsCategory.bird
         bottomWall.physicsBody?.contactTestBitMask = physicsCategory.bird
-        bottomWall.physicsBody?.dynamic = false
+        bottomWall.physicsBody?.isDynamic = false
         bottomWall.physicsBody?.affectedByGravity = false
         
         wallPair.addChild(topWall)
@@ -129,11 +129,11 @@ class GameScene: SKScene , SKPhysicsContactDelegate {
         
         let randomY = CGFloat(Float(arc4random()) / 0xFFFFFFFF) * CGFloat(max - min ) + CGFloat(min)
         wallPair.position.y = wallPair.position.y + randomY
-        wallPair.runAction(moveAndRemove)
+        wallPair.run(moveAndRemove)
         self.addChild(wallPair)
     }
     
-    func didBeginContact(contact: SKPhysicsContact) {
+    func didBegin(_ contact: SKPhysicsContact) {
         let firstBody = contact.bodyA
         let secondBody = contact.bodyB
         if (firstBody.categoryBitMask == physicsCategory.bird && secondBody.categoryBitMask == physicsCategory.scoreNode) || (firstBody.categoryBitMask == physicsCategory.scoreNode && secondBody.categoryBitMask == physicsCategory.bird){
